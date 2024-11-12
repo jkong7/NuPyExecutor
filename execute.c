e329901523f938600e9f36ceaf07f22da1d39659
jkong7 <jonathankong677@gmail.com>
2024-11-11 22:47:57 -0600

    adding original files

---
+++ execute.c
@@ -0,0 +1,41 @@
+/*execute.c*/
+
+//
+// << WHAT IS THE PURPOSE OF THIS FILE??? >>
+//
+// << WHAT IS YOUR NAME >>
+// << WHAT SCHOOL IS THIS >>
+// << WHAT COURSE IS THIS >>
+// << WHAT QUARTER IS THIS >>
+// 
+// Starter code: Prof. Joe Hummel
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>  // true, false
+#include <string.h>
+#include <assert.h>
+
+#include "programgraph.h"
+#include "ram.h"
+#include "execute.h"
+
+
+//
+// Public functions:
+//
+
+//
+// execute
+//
+// Given a nuPython program graph and a memory, 
+// executes the statements in the program graph.
+// If a semantic error occurs (e.g. type error),
+// and error message is output, execution stops,
+// and the function returns.
+//
+void execute(struct STMT* program, struct RAM* memory)
+{
+  printf("\nexecute(): TODO\n\n");
+}
